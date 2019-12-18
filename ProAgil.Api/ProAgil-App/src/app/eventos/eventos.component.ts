5652efae880f441950c16a4f667930abab955ed5
Dione Maciel <dionerm86@gmail.com>
2019-12-18 16:28:00 -0300

    Implementação do Bootstrap e FontAwesome

--- ProAgil.Api/ProAgil-App/src/app/eventos/eventos.component.ts
+++ ProAgil.Api/ProAgil-App/src/app/eventos/eventos.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-eventos',
@@ -7,27 +8,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EventosComponent implements OnInit {
 
-eventos: any = [
-  {
-    EventoId: 1,
-    Tema: 'Angular',
-    Local: 'Belo Horizonte'
-  },
- {
-  EventoId: 2,
-    Tema: '.NetCore',
-    Local: 'Contagem'
-  },
-  {
-    EventoId: 3,
-      Tema: 'Angular & .NetCore',
-      Local: 'Mateus Leme'
-    }
-];
+eventos: any;
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
   ngOnInit() {
+    this.getEventos();
+  }
+
+  getEventos() {
+    this.http.get('http://localhost:5000/api/values').subscribe( response => {
+      this.eventos = response;
+     }, error => {
+       console.log(error);
+     }
+    );
   }
 
 }
--- ProAgil.Api/Startup.cs
+++ ProAgil.Api/Startup.cs
@@ -29,6 +29,7 @@ namespace ProAgil.Api
         {
             services.AddDbContext<DataContext>(x => x.UseSqlite(Configuration.GetConnectionString("DefaultConnection")));
             services.AddControllers();
+            services.AddCors();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -41,6 +42,7 @@ namespace ProAgil.Api
 
             //app.UseHttpsRedirection();
 
+            app.UseCors( x => x.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
             app.UseRouting();
 
             app.UseAuthorization();
