b4a4a5028ddcf33cf878e080decced0a804c7f57
Dione Maciel <dionerm86@gmail.com>
2020-06-09 20:43:31 -0300

    Merge

--- ProAgil.Api/Startup.cs
+++ ProAgil.Api/Startup.cs
@@ -1,4 +1,4 @@
-using System;
+ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -11,7 +11,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
-using ProAgil.Api.Data;
+using ProAgil.Repositorio;
 
 namespace ProAgil.Api
 {
@@ -28,6 +28,7 @@ namespace ProAgil.Api
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddDbContext<DataContext>(x => x.UseSqlite(Configuration.GetConnectionString("DefaultConnection")));
+            services.AddScoped<IProagilRepositorio, ProAgilRepositorio>();
             services.AddControllers();
             services.AddCors();
         }
